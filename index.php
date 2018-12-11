8874e528778e2f7903af77a9680b1ed8a741b04b
andrey_velsk <andrey_velsk@mail.ru>
2018-12-11 14:25:50 +0300

    start making a relevation

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-$value = $_GET['value'];
+function clean_post_data($data){
+    $data = strip_tags($data);
+    $data = strtolower($data);
+    $data = preg_replace('~[^a-z0-9 \x80-\xFF]~i', "",$data); 
+    return $data;
+}
+
+$input_string = clean_post_data($_GET['value']);
+$value = explode(" ", $input_string);
 $xml=simplexml_load_file("yandex.xml") or die("Error: Cannot create object");
 $final=array();
 
@@ -32,6 +40,7 @@ function searchById($xmlstring, $value) {
             $result['minprice'] = $cat_addition['minprice'];
 
             array_push($id_search_result, $result);
+            break;
         }
         $result = null;
     }
@@ -43,6 +52,8 @@ function searchById($xmlstring, $value) {
             $result['price'] = (string)$off->price;
             $result['available'] = (string)$off['available'];
             array_push($id_search_result, $result);
+
+            break;
         }
         $result = null;
     }
@@ -50,55 +61,62 @@ function searchById($xmlstring, $value) {
     return $id_search_result;
 }
 
-function searchByString($xmlstring, $value){
+function searchByString($xmlstring, $values){
     $id_search_result = array();
     foreach ($xmlstring->shop->categories->category as $cat) {
-        $pos = mb_stripos((string)$cat, (string)$value);
-        if ($pos !== false) {
-            echo "Нашел '$value' в '$cat' в позиции $pos <br>";
+        $result=array();
+        $rel = 0;
+        foreach ($values as $value) {
+            $pos = mb_stripos((string)$cat, (string)$value);
+            if ($pos !== false) {
+                $rel++;
+            }
         }
-        $result = null;
+        if ($rel > 0){
+            $result['type'] = 'category';
+            $result['name'] = (string)$cat;
+            $result['catid'] = (string)$cat["id"];
+            $cat_addition = countOffersInCat($xmlstring, intval($cat['id']));
+            $result['count'] = $cat_addition['count'];
+            $result['minprice'] = $cat_addition['minprice'];
+            $result['rel'] = $rel;
+            echo json_encode($result)."<br>"; 
+        }     
+        $result=null;   
     }
 
     foreach ($xmlstring->shop->offers->offer as $off) {
-        $pos = mb_stripos((string)$off->name, (string)$value);
-        if ($pos !== false) {
-            echo "Нашел '$value' в '$off->name' в товаре $pos <br>";
+        $result=array();
+        $rel = 0;
+        foreach ($values as $value) {
+            $pos = mb_stripos((string)$off->name, (string)$value);
+            if ($pos !== false) {
+                $rel++;
+            }
         }
-        $result = null;
+        if ($rel > 0){
+            $result['type'] = 'offer';
+            $result['name'] = (string)$off->name;
+            $result['pic'] = (string)$off->picture;
+            $result['price'] = (string)$off->price;
+            $result['available'] = (string)$off['available'];
+            $result['rel'] = $rel;
+            echo json_encode($result)."<br>"; 
+        }   
     }
 }
 
 searchByString($xml, $value);
-
-if (is_numeric($value)) {
-    $id_search = searchById($xml, $value);
+echo count($value);
+if (count($value) === 1 && is_numeric($value[0])) {
+    $id_search = searchById($xml, $value[0]);
     foreach ($id_search as $res) {
         array_push($final, $res);
-    }    
+    }
 }
 
 $json = json_encode($final); 
 
 echo $json;
-
-/*
-[
-{
-    type: category,
-    name: 'Испарители',
-    offerscount: 32,
-    minprice: 123,
-},
-{
-    type: offer,
-    name: 'Испаритель',
-    pic: pic,
-    price: 1232,
-    available: true
-}
-]
-
-*/
 ?>
 
