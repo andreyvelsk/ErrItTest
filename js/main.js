f4a68f85819ed7c0aa227134aa69d005b57c432f
andrey_velsk <andrey_velsk@mail.ru>
2018-12-12 16:09:19 +0300

    start make delay of input

--- js/main.js
+++ js/main.js
@@ -4,60 +4,64 @@ $(function() {
     $("#form").submit(function(e) {
         e.preventDefault();
     })
-    $search.on('input', function (){
-        console.log($(this).val());
-        if($(this).val()) {
-
-            $.ajax({
-                dataType: 'json',
-                url: 'search.php',
-                data: "value=" + $(this).val(),
-                success: function(jsondata){
-                    console.log(jsondata);
-                    var html = '';
-                    $.each(jsondata, function(key, value){
-                        if(value.type=="offer") {
-                            
-                            html += '<div class="item">';
-
-                            html += '<div class="item_image">';
-                                html += '<img src="' + value.pic + '" alt="' + value.name + '">';
-                            html += '</div>'; // close item-image
-
-                            html += '<div class="item_description">';
-                                html += '<div class="type">Товар </div>';
-                                html += '<div class="name">Наименование: ' + value.name + '</div>';
-                                html += '<div class="price">Цена: ' + value.price + '</div>';
-                            html += '</div>'; // close item-description
-
-                            html += '</div>'; // close item
-                        }
-                        if(value.type=="category") {
-                            
-                            html += '<div class="item">';
-                            
-                            html += '<div class="item_image">';
-                                html += '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Gnome-applications-science.svg/267px-Gnome-applications-science.svg.png" alt="' + value.name + '">';
-                            html += '</div>'; // close item-image
-
-                            html += '<div class="item_description">';
-                                html += '<div class="type">Категория</div>';
-                                html += '<div class="name">Наименование: ' + value.name + '</div>';
-                                html += '<div class="count">Товаров в категории: ' + value.count + '</div>';
-                                html += '<div class="price">Минимальная цена товара: ' + value.minprice + '</div>';
-                            html += '</div>'; // close item-description
-
-                            html += '</div>'; // close item
-                        }
-                    });
-
-                    $('.search-output').html(html);
-                },
-                error: function() {
-                    alert("some error occured");
-                }
-            });
-        }
+    $search.bind('input', function () {
+        var timer, delay = 1000, val = ($(this).val());
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            if(val) {
+                console.log(val);
+                $.ajax({
+                    dataType: 'json',
+                    url: 'search.php',
+                    data: "value=" + val,
+                    success: function(jsondata){
+                        console.log(jsondata);
+                        var html = '';
+                        $.each(jsondata, function(key, value){
+                            if(value.type=="offer") {
+                                
+                                html += '<div class="item">';
+    
+                                html += '<div class="item_image">';
+                                    html += '<img src="' + value.pic + '" alt="' + value.name + '">';
+                                html += '</div>'; // close item-image
+    
+                                html += '<div class="item_description">';
+                                    html += '<div class="type">Товар </div>';
+                                    html += '<div class="name">Наименование: ' + value.name + '</div>';
+                                    html += '<div class="price">Цена: ' + value.price + '</div>';
+                                html += '</div>'; // close item-description
+    
+                                html += '</div>'; // close item
+                            }
+                            if(value.type=="category") {
+                                
+                                html += '<div class="item">';
+                                
+                                html += '<div class="item_image">';
+                                    html += '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Gnome-applications-science.svg/267px-Gnome-applications-science.svg.png" alt="' + value.name + '">';
+                                html += '</div>'; // close item-image
+    
+                                html += '<div class="item_description">';
+                                    html += '<div class="type">Категория</div>';
+                                    html += '<div class="name">Наименование: ' + value.name + '</div>';
+                                    html += '<div class="count">Товаров в категории: ' + value.count + '</div>';
+                                    html += '<div class="price">Минимальная цена товара: ' + value.minprice + '</div>';
+                                html += '</div>'; // close item-description
+    
+                                html += '</div>'; // close item
+                            }
+                        });
+    
+                        $('.search-output').html(html);
+                    },
+                    error: function() {
+                        alert("some error occured");
+                    }
+                });
+            } // end of if($(this).val)
+        }, delay );
+        
                 
     })
 });
