97e6ff14a0dd775e76d585dc7f88cf1f8d229626
andrey_velsk <andrey_velsk@mail.ru>
2018-12-12 11:09:27 +0300

    convert json to html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function() {
     var $search = $("#search");
-    console.log("is loaded");
+
+    $("#form").submit(function(e) {
+        e.preventDefault();
+    })
     $search.on('input', function (){
         console.log($(this).val());
         $.ajax({
@@ -8,7 +11,45 @@ $(function() {
             url: 'search.php',
             data: "value=" + $(this).val(),
             success: function(jsondata){
-              console.log(jsondata);
+                console.log(jsondata);
+                var html = '';
+                $.each(jsondata, function(key, value){
+                    if(value.type=="offer") {
+                        
+                        html += '<div class="item">';
+
+                        html += '<div class="item_image">';
+                            html += '<img src="' + value.pic + '" alt="' + value.name + '">';
+                        html += '</div>'; // close item-image
+
+                        html += '<div class="item_description">';
+                            html += '<div class="type">' + Товар + '</div>';
+                            html += '<div class="name">' + value.name + '</div>';
+                            html += '<div class="price">' + value.price + '</div>';
+                        html += '</div>'; // close item-description
+
+                        html += '</div>'; // close item
+                    }
+                    if(value.type=="category") {
+                        
+                        html += '<div class="item">';
+                        
+                        html += '<div class="item_image">';
+                            html += '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Gnome-applications-science.svg/267px-Gnome-applications-science.svg.png" alt="' + value.name + '">';
+                        html += '</div>'; // close item-image
+
+                        html += '<div class="item_description">';
+                            html += '<div class="type">' + Категория + '</div>';
+                            html += '<div class="name">' + value.name + '</div>';
+                            html += '<div class="count">Товаров в категории: ' + value.count + '</div>';
+                            html += '<div class="price">Минимальная цена товара: ' + value.minprice + '</div>';
+                        html += '</div>'; // close item-description
+
+                        html += '</div>'; // close item
+                    }
+                });
+
+                $('.search-output').html(html);
             },
             error: function() {
                 console.log("some error occured");
